51711ab8a016c074c754c06dd72be080ec77adb6
imora-dev <184550198+imora-dev@users.noreply.github.com>
2024-10-13 05:12:35 -0300

    instalacion de librerias la base de este portfolio se desarrollara con react react es muy bueno para crear interfaces ui sin embargo al utilizar next js puedo tener un work flow mas apropiado sumado a las mutiples ventas del framework incorpore la instalacion inicial de magic ui porque tiene componentes atractivos que planeo utilizar en este proyecto

---
+++ imora-portfoli/src/components/iconcloud.tsx
@@ -0,0 +1,39 @@
+import IconCloud from "@/components/ui/icon-cloud";
+
+const slugs = [
+  "typescript",
+  "javascript",
+  "java",
+  "react",
+  "html5",
+  "css3",
+  "awslambda",
+  "amazonaws",
+  "nginx",
+  "vercel",
+  "git",
+  "github",
+  "springboot",
+  "spring",
+  "springsecurity",
+  "angular",
+  "visualstudiocode",
+  "mongodb",
+  "mognodbatlas",
+  "tailwindcss",
+  "linux",
+  "serverless",
+  "mysql",
+  "python",
+  "apache",
+  "nextdotjs",
+  "nextui"
+]
+
+export function IconCloudDemo() {
+  return (
+    <div className="">
+      <IconCloud iconSlugs={slugs} />
+    </div>
+  );
+}
