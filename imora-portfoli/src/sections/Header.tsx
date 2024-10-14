c191dd23d90d47858cdf942e329338c9b296bcd5
imora-dev <184550198+imora-dev@users.noreply.github.com>
2024-10-14 16:28:39 -0300

    fix:Header cambiamos el z-index de header para que se vea sobre toda la pagina

--- imora-portfoli/src/sections/Header.tsx
+++ imora-portfoli/src/sections/Header.tsx
@@ -1,7 +1,7 @@
 export const Header = () => {
 
     return (
-        <div className="flex  justify-center items-center fixed top-3 w-full " > 
+        <div className="flex  justify-center items-center fixed top-3 w-full z-50 " > 
             <nav className="flex gap-1 p-0.5 border border-white/15 rounded-full bg-white/10 backdrop-blur">
             <a href="#" className="nav-item  bg-white text-gray-900 hover:bg-white/70 hover:text-gray-900">Inicio</a>
             <a href="#" className="nav-item">Proyectos</a>
